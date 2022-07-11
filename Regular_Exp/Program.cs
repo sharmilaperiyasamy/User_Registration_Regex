969a99a38a335eee323d82d390b60c6b8d86f24f
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-11 23:27:27 +0530

    [Sharmila] Lastname Checker

--- Regular_Exp/Program.cs
+++ Regular_Exp/Program.cs
@@ -1,5 +1,21 @@
 ﻿Regular_Exp.Regular_Expressions reg = new Regular_Exp.Regular_Expressions();
-Console.WriteLine("Enter the First Name : ");
-string fname = Console.ReadLine();
 
-reg.FirstNameCheck(fname);
+Console.WriteLine("Regular Expressions : \n0.exit\n1.Firstname Checker\n2.Lastname Checker\nEnter your option");
+int n = Convert.ToInt32(Console.ReadLine());
+switch(n)
+{
+    case 1:
+        Console.WriteLine("Enter the First Name : ");
+        string fname = Console.ReadLine();
+        reg.FirstNameCheck(fname);
+        break;
+        case 2:
+        Console.WriteLine("Enter the Last Name : ");
+        string lname = Console.ReadLine();
+        reg.LastNameCheck(lname);
+        break;
+    default:
+        Console.WriteLine("Invalid option");
+        break;
+
+}
--- Regular_Exp/Regular_Expressions.cs
+++ Regular_Exp/Regular_Expressions.cs
@@ -9,15 +9,26 @@ namespace Regular_Exp
 {
     internal class Regular_Expressions
     {
-        const string NAME = "^[A-Z]{1}[a-z]{2,}$";
+        const string First_name = "^[A-Z]{1}[a-z]{2,}$";
+        const string Last_name = "^[A-Z]{1}[a-z]{2,}$";
         public void FirstNameCheck(string firstName)
         {
-            if (Regex.IsMatch(firstName, NAME))
+            if (Regex.IsMatch(firstName, First_name))
             {
                 Console.WriteLine("First name is valid.");
                 return;
             }
             Console.WriteLine("First name is not valid.");
+        }
+
+        public void LastNameCheck(string lastname)
+        {
+            if(Regex.IsMatch(lastname, Last_name))
+            {
+                Console.WriteLine("Last name is valid.");
+                return;
+            }
+            Console.WriteLine("Lastname is not valid");
         }
     }
 }
