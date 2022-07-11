51f8033ca0899486b3c1e3188b094691ae2176a1
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-11 23:16:24 +0530

    [Sharmila] uc1 firstname check

---
+++ Regular_Exp/Program.cs
@@ -0,0 +1,5 @@
+﻿Regular_Exp.Regular_Expressions reg = new Regular_Exp.Regular_Expressions();
+Console.WriteLine("Enter the First Name : ");
+string fname = Console.ReadLine();
+
+reg.FirstNameCheck(fname);
---
+++ Regular_Exp/Regular_Expressions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regular_Exp
+{
+    internal class Regular_Expressions
+    {
+        const string NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public void FirstNameCheck(string firstName)
+        {
+            if (Regex.IsMatch(firstName, NAME))
+            {
+                Console.WriteLine("First name is valid.");
+                return;
+            }
+            Console.WriteLine("First name is not valid.");
+        }
+    }
+}
